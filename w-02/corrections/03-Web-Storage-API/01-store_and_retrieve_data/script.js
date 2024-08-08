22b5461359710f30af09e912bd0aa88ff11b3935
mariblan <mariablancocanal@gmail.com>
2024-08-08 10:30:23 +0200

    Added lecture storage api and corrections dom

---
+++ w-02/corrections/03-Web-Storage-API/01-store_and_retrieve_data/script.js
@@ -0,0 +1,15 @@
+// You can work here or download the template
+
+// 1. Store data:
+// From the boilerplate, get the contents of the input once the form is submitted.
+// Make sure the input is not empty before saving!
+// Store this value in localStorage as part of an array. Store the new value at the beginning of the array!
+// Reset the form
+
+// 2. Retrieve data:
+// Every time you hit the "Submit" button, add a new list item to the empty ul. Make sure the new item is at the top!
+// Make sure that when the document loads, you populate the contents of your storage item into the list. You can try to add an event listener of type load to the window itself!
+
+// 3. Reload:
+// Just because we can. Add an event to the reload button so it reloads the preview. Your store data should reload too!
+// Tip: Verify the Storage section in the browser's developer tools to make sure the data is there!
---
+++ w-02/corrections/04-Fetch-API/01-fetch_get_data_and_add_to_dom/script.js
@@ -0,0 +1,9 @@
+// You can work here or download the template
+
+// 1. Fetch Data: Write JavaScript to fetch todos from the JSON
+// Placeholder API, we want the ToDos endpoint, you can find
+// more about the API docs in here.
+
+// 2. Display Data: Populate the DOM with the fetched todos.
+// Pay attention to the completed status of each task in the
+// response and style it accordingly!
